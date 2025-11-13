4f60cc77f258964cdd1c6672a86c64c674fbb02d
Jussi Rajala <zemm@iki.fi>
2025-11-13 16:36:03 +0200

    Reviung: tweak symbol layer. All: move common defines to lib

---
+++ config/lib/base.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//
+// Common defines
+//
+
+// Modifiers for left/right side
+#define LM1 LGUI
+#define LM2 LALT
+#define LM3 LSHFT
+#define LM4 LCTRL
+#define RM1 RCTRL
+#define RM2 RSHFT
+#define RM3 LALT
+#define RM4 RGUI
+
+// Shortenings
+#define KP_MUL KP_MULTIPLY
+#define KP_DIV KP_DIVIDE
+#define EURO RA(N5)
+#define ___
